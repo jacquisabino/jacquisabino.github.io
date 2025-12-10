935f638d032f46f6dcbeda3d27829ec75bbe097c
jacqui <jacqui_sabino@icloud.com>
2025-12-09 23:08:48 -0600

    added javascript to hrefs and empty html files

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function displayEducation(){
+    window.location.href = "education.html";
+}
+
+function displayWork(){
+    window.location.href = "work.html";
+}
+
+function displayContact(){
+    window.location.href = "contact.html";
+}
+
+function displayProjects(){
+    window.location.href = "projects.html";
+}
